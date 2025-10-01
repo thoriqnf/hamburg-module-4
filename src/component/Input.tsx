f97961f538467652f1d874e309e731a1c7ab1b3d
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2025-10-01 20:29:43 +0700

    feat: adding post method

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -14,8 +14,27 @@ function Input() {
     formState: { errors },
   } = useForm<ProductForm>();
 
-  const onSubmitFunction = (values: ProductForm) => {
+  const onSubmitFunction = async (values: ProductForm) => {
     console.log("values", values);
+
+    try {
+      const response = await fetch(
+        "https://64ca45bd700d50e3c7049e2f.mockapi.io/product",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            title: values.title,
+            price: values.price,
+            stock: values.stock,
+          }),
+        }
+      );
+
+      if (!response.ok) throw new Error("post failed");
+      const data = await response.json();
+      console.log("data", data);
+    } catch (e) {}
   };
 
   return (
@@ -31,6 +50,7 @@ function Input() {
         />
         {errors.title && <p>{errors.title.message}</p>}
         <input
+          type="number"
           {...register("price", {
             required: "price required",
             validate: (item) => Number(item) >= 0 || "price must be >= 0",
