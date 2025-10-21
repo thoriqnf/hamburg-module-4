afc3200a2b6cf4d9785d4c79fcd63633e056cc45
Thoriq Nur Faizal <thoriqnfaizal@gmail.com>
2025-10-21 13:25:03 +0700

    feat adding material day 1

---
+++ jest.config.ts
@@ -0,0 +1,71 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "mts",
+    "cts",
+    "tsx",
+    "json",
+    "node"
+  ],
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  // The test environment that will be used for testing
+  testEnvironment: "jest-environment-jsdom",
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.?([mc])[jt]s?(x)",
+    "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"
+  ],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/.next/",
+    "/out/"
+  ],
+
+  // A map from regular expressions to paths to transformers
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "\\.pnp\\.[^\\/]+$"
+  ],
+};
+
+export default config;
---
+++ src/__tests__/random.test.js
@@ -0,0 +1,26 @@
+test("ini test pertama", () => {
+  expect(1 + 1).toBe(2); // happy path / positive case
+  expect(1 + 1).not.toBe(1); // kita mau negative case, artinya wajib salah
+});
+
+test("deep equal", () => {
+  expect({ a: "1", b: 2 }).toEqual({ b: 2, a: "1" });
+});
+
+// toBeDefined ini adalah cheat code
+// karena tugas dia cmn mengecheck ada atau tidak
+test("to be defined", () => {
+  expect("a").toBeDefined();
+});
+
+test("to be undefined", () => {
+  expect(undefined).toBeUndefined();
+});
+
+test("apakah saya benar", () => {
+  expect(true).toBeTruthy();
+});
+
+test("apakah saya salah", () => {
+  expect(false).toBeFalsy();
+});
